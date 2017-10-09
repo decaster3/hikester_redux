dc72580614b5624c164966821bb26b0becdddea3
Ilgiz <ilgiz1996@mail.ru>
2017-10-10 01:18:57 +0300

    fixing map, add to container

--- src/reducers/events_creation/initial_state.js
+++ src/reducers/events_creation/initial_state.js
@@ -2,6 +2,8 @@
 module.exports = {
   new_event: {
     tag: null,
-    location: null
+    location: { lat: 55.7529120574368, lng: 48.743462562561035},
+    defaultCenter: { lat: 55.7529120574368, lng: 48.743462562561035 },
+    defaultZoom: 16
   }
 }
