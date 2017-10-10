030d5b9c1225120565d7b988f092628c8b771c9b
Rinat Katipov <rinat777hatipov@gmail.com>
2017-10-11 00:59:58 +0300

    added notifications

--- src/containers/profile/notifications/notifications_container.js
+++ src/containers/profile/notifications/notifications_container.js
@@ -1,50 +1,44 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
-import { fromRelevantToOld, fromIdToEvent } from '../../../actions/notifications/notifications_actions'
+import { getNotifications, fromIdToEvent } from '../../../actions/notifications/notifications_actions'
 
 class NotificationsContainer extends Component {
 
   componentDidMount(){
-    let p = this.props
-    if (this.props.user.currently != "ANONYMOUS"){
-      console.log(1);
-      fromRelevantToOld()
-      var notifications = p.user.notifications.old
-      Object.keys(notifications).map((key) =>{
-        console.log(notifications[key]);
-        p.fromIdToEvent(notifications[key])
-      })
-    }
-    else (
-      this.componentDidMount()
-    )
+    this.props.getNotifications()
   }
-//   container = p.notifications.notifications.map((not, index) => {
-//   return (
-//   <div key = {index}>
-//     Event <br />
-//     <p>address: {not.address}</p>
-//     <p>cost: {not.cost}</p>
-//     <p>end_time: {not.end_time}</p>
-//     <p>start_time: {not.start_time}</p>
-//     <p>description: {not.description}</p>
-//     <p>lat: {not.lat}</p>
-//     <p>lng: {not.lng}</p>
-//     <p>name: {not.name}</p>
-//     <p>max_people_count: {not.max_people_count}</p>
-//     <p>start_date: {not.start_datee}</p>
-//     <p>tag: {not.tag}</p>
-//   </div>)
-// })
-
   render(){
-    // let p = this.props
-      return(
-        <div>
-          hey
+      let p = this.props
+      var view = {}
+      if (p.user.currently != "ANONYMOUS"){
+        if(p.notifications.notifications.length > 0){
+          console.log(123);
+          view = p.notifications.notifications.map((not, index) => {
+            return(<div key = {index}>
+              {not.name}
+            </div>)
+          })
+        console.log(view);
+        return(
+          <div>
+          {view}
         </div>
-      )
+        )
+        }
+        else {
+          return(<div>
+            You havent got notifications
+          </div>)
+        }
+      }
+      else {
+        return(
+          <div>
+            LOADING
+          </div>
+        )
+      }
     }
 }
 function mapStateToProps(state){
@@ -57,7 +51,7 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch){
   return bindActionCreators(
     {
-      fromIdToEvent: fromIdToEvent
+      getNotifications: getNotifications
     },
     dispatch
   )
