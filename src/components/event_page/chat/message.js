7d0e8f14085e022a727c28ab63fb799183050897
Ilgiz <ilgiz1996@mail.ru>
2017-10-12 06:32:59 +0300

    add

---
+++ src/components/event_page/chat/message.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+export default class Message extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    let active_message = this.props.active ? ' active' : '';
+    return (
+      <div className={"message-item d-flex justify-content-center" + active_message}>
+        <div className="message-person-photo-wrapper">
+          <img src={this.props.photoURL} className="message-person-photo" />
+        </div>
+        <div className="message-wrapper">
+          <div className="message-person">
+            {this.props.autor}
+          </div>
+          <div className="message-text">
+            {this.props.text}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
