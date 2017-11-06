1977663fc1a370b82436c9b4d69c901a34dc56aa
Anvar <anvarzkr@gmail.com>
2017-11-06 14:40:55 +0300

    + infowindow design

--- src/components/map/my_marker.js
+++ src/components/map/my_marker.js
@@ -22,15 +22,47 @@ class MyMarker extends Component {
 
   render() {
     var history = this.props.history;
+    let image = this.props.event.photoUrl || "/assets/images/questroom.jpg";
     return (<Marker
       position={this.props.location}
       onClick={this.onToggleOpen}>
-      {this.state.isInfoShown && <InfoWindow onCloseClick={this.onToggleOpen}>
-        <div>
-          <p onClick={() => history.push('/event/' + this.props.event.id)}>link </p>
-          <p>{this.props.event.name} </p>
-        </div>
-      </InfoWindow>
+      {this.state.isInfoShown &&
+        <InfoWindow onCloseClick={this.onToggleOpen} className="infwndw">
+          <div className="event-info-window">
+            <div className="profile-event-name text-center" onClick={() => history.push('/event/' + this.props.event.id)}>
+              {this.props.event.name}
+            </div>
+            <div className="event-item-image mb-2">
+              <img src={image} />
+            </div>
+            <div className="profile-event-description">
+              <div className="profile-event-info row">
+                <div className="profile-event-info-name col-3">
+                  Date
+                </div>
+                <div className="profile-event-info-value col-9">
+                  {this.props.event.date}
+                </div>
+              </div>
+              <div className="profile-event-info row">
+                <div className="profile-event-info-name col-3">
+                  Cost
+                </div>
+                <div className="profile-event-info-value col-9">
+                  {this.props.event.cost}
+                </div>
+              </div>
+              <div className="profile-event-info row">
+                <div className="profile-event-info-name col-3">
+                  Description
+                </div>
+                <div className="profile-event-info-value col-9">
+                  {this.props.event.description}
+                </div>
+              </div>
+            </div>
+          </div>
+        </InfoWindow>
       }
     </Marker>
     )
