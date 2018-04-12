479533a5a7a2978f9904cec38f959bf9abe8e4e9
Rinat Katipov <rinat777hatipov@gmail.com>
2018-04-12 08:50:36 +0300

    qwe

--- src/actions/events_creation/events_creation.js
+++ src/actions/events_creation/events_creation.js
@@ -101,7 +101,58 @@ export function createNewEvent(address, cost, start_date, end_date, description,
        })
    }
   }).catch((err) => {
-    console.log(err);
+    start_date = new Date(start_date.toDate().getTime());
+    end_date = new Date(end_date.toDate().getTime());
+    let user = firebase.auth().currentUser;
+
+    let creatorEventsRef = firebase.database().ref().child('users').child(user.uid).child('my_events')
+    let userAttendsEventsRef = firebase.database().ref().child('users').child(user.uid).child('events')
+    //firestore db
+    var db = firebase.firestore();
+    var fireStoreEventsRef = db.collection("events")
+
+      var pushh = creatorEventsRef.push()
+      var key = pushh.key
+      //location not yet
+      pushh.set(
+        key
+      ).then( () =>
+        {userAttendsEventsRef.push(key)}
+      ).then( () => {
+        fireStoreEventsRef.doc(key).set({
+              address,
+              cost,
+              end_date,
+              start_date,
+              description,
+              creator: firebase.auth().currentUser.uid,
+              lat: getState().new_event.location.lat,
+              lng: getState().new_event.location.lng,
+              name,
+              max_people_count,
+              people_in_event: 1,
+              spam_counter: 0,
+              tag: getState().new_event.tag})
+          .then( () => {
+            let user = firebase.auth().currentUser
+            firebase.database().ref().child('users').child(user.uid).once("value", user => {
+              var userFromDB = user.val()
+              userFromDB["uid"] = user.key
+              firebase.firestore().collection("events").doc(key).collection("users").doc(user.uid).set(
+                userFromDB
+              )
+            })
+          })
+          .then(function() {
+              console.log("Document successfully written!");
+                notification_success("Event was successfully created!")
+                dispatch(push('/event/' + key))
+              // }
+          })
+          .catch(function(error) {
+              console.error("Error writing document: ", error);
+          });
+      })
   })
   }
 }
